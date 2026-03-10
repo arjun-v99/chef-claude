0b36e07625b0d450093a67b9045b4984700bd60b
Arjun Raj <arjunrajvilakkat@outlook.com>
2026-03-10 14:49:11 +0530

    changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,32 @@
 import "./Main.css";
+import { useState } from "react";
 
 export default function Main() {
-  function handleBtnClick() {
-    console.log("Clicked");
+  const ingrediants = [];
+  const ingrediantsListItems = ingrediants.map((ingrediant) => (
+    <li key={ingrediant}>{ingrediant}</li>
+  ));
+
+  function addIngrediant(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngrediant = formData.get("ingrediantName");
+    ingrediants.push(newIngrediant);
   }
+
   return (
     <main className="main-container">
-      <form className="form-container">
+      <form className="form-container" onSubmit={addIngrediant}>
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add Ingrediant"
           className="form-control"
+          name="ingrediantName"
         />
-        <button type="button" className="btn-primary" onClick={handleBtnClick}>
-          Add Ingrediant
-        </button>
+        <button className="btn-primary">Add Ingrediant</button>
       </form>
+      <ul>{ingrediantsListItems}</ul>
     </main>
   );
 }
