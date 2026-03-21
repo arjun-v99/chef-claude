4e18de81acb5a25a8e7adb0e372fc4b8b99a3ad2
Arjun Raj <arjunrajvilakkat@outlook.com>
2026-03-21 15:12:58 +0530

    even more destructuring

--- src/components/IngredientsList/IngredientsList.jsx
+++ src/components/IngredientsList/IngredientsList.jsx
@@ -1,4 +1,22 @@
-export function IngredientsList({ ingrediant }) {
-  console.log(ingrediant);
-  return <li>{ingrediant}</li>;
+export function IngredientsList({ ingrediants, setRecipeRecievedStatus }) {
+  const ingrediantsListItems = ingrediants.map((ingrediant) => (
+    <li>{ingrediant}</li>
+  ));
+  return (
+    <section>
+      <h2>Ingredients on the hand:</h2>
+      <ul className="ingredients-list">{ingrediantsListItems}</ul>
+      {ingrediants.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients</p>
+          </div>
+          <div>
+            <button onClick={setRecipeRecievedStatus}>Get a Recipe</button>
+          </div>
+        </div>
+      )}
+    </section>
+  );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,10 +7,6 @@ export default function Main() {
   let [ingrediants, setIngrediant] = useState([]);
   let [recipeRecieved, setRecipeRecieved] = useState(false);
 
-  const ingrediantsListItems = ingrediants.map((ingrediant, index) => (
-    <IngredientsList key={index} ingrediant={ingrediant}></IngredientsList>
-  ));
-
   function addIngrediant(formData) {
     const newIngrediant = formData.get("ingrediantName");
     setIngrediant((prev) => [...prev, newIngrediant]);
@@ -33,21 +29,10 @@ export default function Main() {
         <button className="btn-primary">Add Ingrediant</button>
       </form>
       {ingrediants.length > 0 && (
-        <section>
-          <h2>Ingredients on the hand:</h2>
-          <ul className="ingredients-list">{ingrediantsListItems}</ul>
-          {ingrediants.length > 3 && (
-            <div className="get-recipe-container">
-              <div>
-                <h3>Ready for a recipe?</h3>
-                <p>Generate a recipe from your list of ingredients</p>
-              </div>
-              <div>
-                <button onClick={setRecipeRecievedStatus}>Get a Recipe</button>
-              </div>
-            </div>
-          )}
-        </section>
+        <IngredientsList
+          ingrediants={ingrediants}
+          setRecipeRecievedStatus={setRecipeRecievedStatus}
+        />
       )}
       {recipeRecieved && <ClaudeRecipe></ClaudeRecipe>}
     </main>
