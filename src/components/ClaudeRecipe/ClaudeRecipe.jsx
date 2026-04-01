874fa82d499cc3a1a6a9c33dc2206bccc786a823
Arjun Raj <arjunrajvilakkat@outlook.com>
2026-04-01 14:52:55 +0530

    mdn standardisation

--- src/components/ClaudeRecipe/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe/ClaudeRecipe.jsx
@@ -2,7 +2,7 @@ import "./ClaudeRecipe.css";
 import Markdown from "react-markdown";
 export function ClaudeRecipe({ aiResponse }) {
   return (
-    <section className="suggested-recipe-container">
+    <section className="suggested-recipe-container" aria-live="polite">
       <Markdown>{aiResponse}</Markdown>
     </section>
   );
